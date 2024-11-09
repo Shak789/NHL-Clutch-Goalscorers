cbec3c8a3c61058779ef76b458051511758f2687
Shakeel Jivraj <shakjivraj87@gmail.com>
2024-11-08 23:29:07 -0500

    sdfdsf

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,7 +3,7 @@ class Solution:
         solution = []
         for i in range(0, n + 1):
             if ((i + 1) % 3 == 0 and (i + 1) % 5 == 0 ):
-                soution[i] = "FizzBsdfsdfuzz"
+                soution[i] = "FizzBsdfsdfdfuzz"
             elif ((i + 1) % 3 == 0):
                 soution[i] = "Fizz"
             elif ((i + 1) % 5 == 0):
